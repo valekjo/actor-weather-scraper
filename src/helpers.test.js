dde5c20ca54fde3f2eb6a4877fe9f516a30e191e
Josef Válek <valek.josef@seznam.cz>
2020-08-12 10:11:27 +0200

    added test

--- src/helpers.test.js
+++ src/helpers.test.js
@@ -1,18 +1,59 @@
 const helpers = require('./helpers');
 
+test('valid object of arrays is converted', () => {
+    const data = [
+        {
+            input: {
+                a: [1, 2, 3],
+                b: [4, 5, 6],
+            },
+            output: [
+                { a: 1, b: 4 },
+                { a: 2, b: 5 },
+                { a: 3, b: 6 },
+            ],
+        },
+        {
+            input: {
+                a: [1],
+                b: [2],
+            },
+            output: [{ a: 1, b: 2 }],
+        },
+    ];
+    data.forEach(({ input, output }) => {
+        expect(helpers.objectOfArraysToArrayOfObjects(input)).toEqual(output);
+    });
+});
+
+test('invalid object of arrays throws error', () => {
+    const data = [
+        {
+            a: [1, 2, 3],
+            b: [1, 2],
+        },
+        {},
+    ];
+    data.forEach((input) => {
+        expect(() => {
+            helpers.objectOfArraysToArrayOfObjects(input);
+        }).toThrow();
+    });
+});
+
 test('location id is parsed properly', () => {
     const data = [
         {
-            url: 'https://weather.com/cs-CZ/weather/today/l/d59971d16b1b808fd775cf6f51716cfffc26979b3935a567a92ce63ef3d4e41c',
-            locationId: 'd59971d16b1b808fd775cf6f51716cfffc26979b3935a567a92ce63ef3d4e41c',
+            url:
+                'https://weather.com/cs-CZ/weather/today/l/d59971d16b1b808fd775cf6f51716cfffc26979b3935a567a92ce63ef3d4e41c',
+            locationId:
+                'd59971d16b1b808fd775cf6f51716cfffc26979b3935a567a92ce63ef3d4e41c',
         },
         {
-            url: 'https://weather.com/cs-CZ/weather/monthly/l/d59971d16b1b808fd775cf6f51716cfffc26979b3935a567a92ce63ef3d4e41c',
-            locationId: 'd59971d16b1b808fd775cf6f51716cfffc26979b3935a567a92ce63ef3d4e41c',
-        },
-        {
-            url: 'https://weather.com/cs-CZ/weather/monthly/l/d59971d16b1b808fd775cf6f51716cfffc26979b3935a567a92ce63ef3d4e41c#test',
-            locationId: 'd59971d16b1b808fd775cf6f51716cfffc26979b3935a567a92ce63ef3d4e41c',
+            url:
+                'https://weather.com/cs-CZ/weather/monthly/l/d59971d16b1b808fd775cf6f51716cfffc26979b3935a567a92ce63ef3d4e41c#test',
+            locationId:
+                'd59971d16b1b808fd775cf6f51716cfffc26979b3935a567a92ce63ef3d4e41c',
         },
     ];
     data.forEach(({ url, locationId }) => {
